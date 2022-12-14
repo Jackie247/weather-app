460637988a4b2092c10f283b1af0fcc8a6849bce
Jackie Yu <jackieyu247@gmail.com>
2022-12-14 23:42:07 +0000

    add try block and catch block

--- src/js/modules/weatherAPI.ts
+++ src/js/modules/weatherAPI.ts
@@ -1,9 +1,16 @@
-async function getWeather(location: string) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=733ac34f981d37338adc9bcd9ffb88cb`,
-    {
-      mode: "cors",
-    }
-  );
-  const weatherData = await response.json();
+export async function getWeather(location: string) {
+  try {
+    // Make a request to the API and returns a promise. Pending state
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=733ac34f981d37338adc9bcd9ffb88cb`,
+      {
+        mode: "cors",
+      }
+    );
+    // We store the fetched API data in weatherData variable. Resolved state
+    const weatherData = await response.json();
+  } catch (error) {
+    // Rejected state, API data was unable to be retrieved.
+    throw error;
+  }
 }
