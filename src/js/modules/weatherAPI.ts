9b850de2a16d46b26138a3c017125b966e874099
Jackie Yu <jackieyu247@gmail.com>
2022-12-14 23:49:55 +0000

    change from async and await code to use promise syntax

--- src/js/modules/weatherAPI.ts
+++ src/js/modules/weatherAPI.ts
@@ -1,18 +1,19 @@
-export async function getWeather(location: string) {
-  try {
+export function getWeather(location: string) {
+  // We store the fetched API data in weatherData variable. Resolved state
+  return new Promise((resolve, reject) => {
     // Make a request to the API and returns a promise. Pending state
-    const response = await fetch(
+    fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=733ac34f981d37338adc9bcd9ffb88cb`,
       {
         mode: "cors",
       }
-    );
-    // We store the fetched API data in weatherData variable. Resolved state
-    const weatherData = await response.json();
-    return weatherData;
-  } catch (error) {
-    // Rejected state, API data was unable to be retrieved.
-    console.error("There was a problem with the request:", error);
-    throw error;
-  }
+    )
+      .then((response) => response.json())
+      .then((weatherData) => resolve(weatherData))
+      .catch((error) => {
+        // Rejected state, API data was unable to be retrieved.
+        console.error("There was a problem with the request:", error);
+        reject(error);
+      });
+  });
 }
