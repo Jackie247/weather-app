2858e80ea953e90c927e92d672bb69d0e672bbc6
Jackie Yu <jackieyu247@gmail.com>
2022-12-15 21:03:15 +0000

    update index

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,7 @@
 import "../style/styles.scss";
 import UI from "./modules/ui.js";
+import weatherAPI from "./modules/weatherAPI";
 
 console.log("hello");
+
+const searchLocationInput = document.getElementById("search-location-input");
--- src/js/modules/weatherAPI.ts
+++ src/js/modules/weatherAPI.ts
@@ -1,15 +1,7 @@
-// Define our interface for the API data
-interface WeatherData {
-  cityName: string;
-  temperature: number;
-  feelsLike: number;
-  humidity: number;
-  windSpeed: number;
-}
 // Create the weather constructor function
 const Weather = (() => {
-  // Convert raw data from API into weatherData format
-  function parseData(data: any): WeatherData {
+  // Convert raw data from API and get the fields we want.
+  function parseData(data: any) {
     const {
       name: cityName,
       main: { temp: temperature, feels_like: feelsLike, humidity },
@@ -18,17 +10,17 @@ const Weather = (() => {
     return { cityName, temperature, feelsLike, humidity, windSpeed };
   }
 
-  function getWeatherData(location: string): Promise<WeatherData | null> {
+  function getWeatherData(location: string) {
     return new Promise((resolve, reject) => {
+      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=733ac34f981d37338adc9bcd9ffb88cb`;
       // Make a request to the API and returns a promise. Pending state
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=733ac34f981d37338adc9bcd9ffb88cb`,
-        {
-          mode: "cors",
-        }
-      )
-        .then((response) => response.json()) // We store the fetched API data in weatherData variable.
-        .then((weatherData) => resolve(weatherData)) // the promise is resolved with the API response data
+      fetch(apiURL, {
+        mode: "cors",
+      })
+        .then((response) => parseData(response.json())) // We store the fetched API data in weatherData variable.
+        .then((weatherData) => {
+          resolve(weatherData); // the promise is resolved with the API response data
+        })
         .catch((error) => {
           // Rejected state, API data was unable to be retrieved.
           console.error(`Location "${location}" not found`, error);
