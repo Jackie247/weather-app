3f3767d6e2dca49145f733a1d353f84259f68acb
Jackie Yu <jackieyu247@gmail.com>
2022-12-18 15:01:21 +0000

    update index

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import "../style/styles.scss";
 import weatherAPI from "./weatherAPI";
+import ui from "./ui";
 
 console.log("hello");
 
@@ -7,9 +8,17 @@ const searchLocationInput = document.getElementById("search-location-input");
 const searchForm = document.getElementById("search-form");
 const searchButton = document.getElementById("search-btn");
 
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
 searchButton.addEventListener("click", async () => {
-  if (searchLocationInput.value == "") return;
+  if (searchLocationInput.value == "") {
+    return;
+  }
   const weatherData = await weatherAPI.getWeatherData(
     searchLocationInput.value
   );
+  console.log(weatherData);
+  ui.updateMainDisplay(weatherData);
 });
